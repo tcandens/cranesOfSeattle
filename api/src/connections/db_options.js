99ffd1a9b14b999badb8635a9c25e946c2f1fa2c
tcandens <joey.thies@gmail.com>
2016-01-06 16:19:38 -0800

    Update connections and server with proper es6 module syntax and promises/generator usage within routes

--- api/src/connections/db_options.js
+++ api/src/connections/db_options.js
@@ -1,7 +1,17 @@
-export var options = {
+const options = {
   connect: (client) => {
     var info = client.connectionParameters;
     console.log('Connected to database:' + info.database);
+  },
+  error: (err, e) => {
+    console.log('Error:', err);
+    if (e.query) {
+      console.log('Query:', e.query);
+      if (e.params) {
+        console.log('Parameters:', e.params);
+      }
+    }
   }
 };
 
+export default options
