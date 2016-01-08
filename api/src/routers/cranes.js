5803c0a8ab4df8d7de391f4a5eeb5abf6a698f98
tcandens <joey.thies@gmail.com>
2016-01-07 17:34:39 -0800

    Move crane routes out of server.js
    Includes changes to eslint rules. Abstraction of router allows for
    the separation of resources.

---
+++ api/src/routers/cranes.js
@@ -0,0 +1,16 @@
+import Router from 'koa-router'
+import db from '../connections/db'
+
+export default Router()
+  .get('/', function *() {
+    this.body = { message: 'Hello there!' };
+  })
+  .get('/cranes', function *() {
+    let cranes = yield db.any('SELECT name, id FROM cranes');
+    this.body = cranes;
+  })
+  .post('/cranes', function *() {
+    let crane = this.request.body;
+    let id = yield db.one('INSERT into cranes(name) values($1) returning id', [crane.name]);
+    this.body = id;
+  })
--- api/src/server.js
+++ api/src/server.js
@@ -1,11 +1,9 @@
 import koa from 'koa'
-import Router from 'koa-router'
 import logger from 'koa-logger'
 import json from 'koa-json'
 import parseJson from 'koa-parse-json'
-import db from './connections/db'
 
-let router = Router();
+import cranes from './routers/cranes'
 
 // Create table
 //db.none('CREATE TABLE cranes (id serial, name varchar (12))')
@@ -21,23 +19,8 @@ app.use(parseJson());
 app.use(json());
 app.use(logger());
 
-router.get('/', function *() {
-  this.body = { message: 'Hello there!' };
-});
-
-router.get('/cranes', function *(next) {
-  let cranes = yield db.any('SELECT name, id FROM cranes');
-  this.body = cranes;
-});
-
-router.post('/cranes', function *(next) {
-  let crane = this.request.body;
-  let id = yield db.one('INSERT into cranes(name) values($1) returning id', [crane.name]);
-  this.body = id;
-});
-
 app
-  .use(router.routes())
-  .use(router.allowedMethods());
+  .use(cranes.routes())
+  .use(cranes.allowedMethods());
 
 app.listen(PORT);
