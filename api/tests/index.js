0d7ab2f1c2c452839c0470b79fad68f03d0e7b42
tcandens <joey.thies@gmail.com>
2016-01-12 17:10:53 -0800

    After many tweaks, Koa app/server is still leaking handlers.
    This is making supertest hang in tests/.

--- api/tests/index.js
+++ api/tests/index.js
@@ -1,13 +1,26 @@
+/**
+ * Environmental Variables
+ */
+// process.env.PGDATABASE = 'test'
+/**
+ * Dependencies
+ */
+import leaks from 'leaked-handles'
 import test from 'tape-dispenser'
-import { server } from '../src/server'
 import supertest from 'co-supertest'
+// import db from '../connections/db'
+import { app } from '../src/app'
+
+// See TODO: API->testing
+// TODO: Run migrations on test database
 
 const request = supertest.agent(server);
 
-test('First test!', function *(assert) {
+test('First test!', {timeout: 2000}, function *(assert) {
   const res = yield request
-    .get('/')
+    .get('/cranes')
     .expect(200)
     .end()
-  assert.equal(res.text, 'Hello there');
+  assert.ok(res, 'Response from /cranes');
+  assert.timeoutAfter(2000);
 });
