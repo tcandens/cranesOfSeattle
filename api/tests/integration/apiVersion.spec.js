e97400962266054d105441f8502f00ab8082fab0
tcandens <joey.thies@gmail.com>
2016-01-22 13:07:14 -0800

    Add test for API versioning field in headers.

---
+++ api/tests/integration/apiVersion.spec.js
@@ -0,0 +1,30 @@
+/**
+ * Dependencies
+ */
+require('leaked-handles').set({
+  fullStack: true
+});
+import test from 'tape-dispenser'
+import supertest from 'co-supertest'
+import server from '../../src/server'
+import packageJSON from '../../package.json'
+
+const request = supertest.agent(server);
+
+const currentVersion = JSON.parse(packageJSON).version;
+
+test('CHECKING API VERSION IS IN RESPONSE HEADERS', function *(assert) {
+  const response = request
+    .get('/')
+    .expect(200)
+    .end();
+
+  assert.equal(
+    response.header.version,
+    currentVersion,
+    'Should return header field with version matching current version.'
+  );
+  
+});
+
+server.close();
