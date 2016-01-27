eac751a4c7e0012034df976da5a4950b3aef92cb
tcandens <joey.thies@gmail.com>
2016-01-26 16:02:46 -0800

    Add test to run-tests.sh to prevent unneeded container rebuilds.

--- api/run-tests.sh
+++ api/run-tests.sh
@@ -3,8 +3,16 @@
 # Test if Docker-compose command is available
 command -v docker-compose>/dev/null 2>&1 || { echo "Error: Docker-compose is
 required. Please visit https://docs.docker.com/compose/"; exit 1; }
+
 # Docker-compose build image for testing
-echo "> Building from test-docker-compose.yml"
-docker-compose -f test-docker-compose.yml build
+# Test for existing docker containers
+containers=`docker ps -a |& grep -c "api_test"`
+if [ $containers -le "1" ]; then
+  echo "> Building from test-docker-compose.yml"
+  docker-compose -f test-docker-compose.yml build
+else
+  echo "> Containers are build and/or running."
+fi
+
 echo "> Booting containers"
 docker-compose -f test-docker-compose.yml up
