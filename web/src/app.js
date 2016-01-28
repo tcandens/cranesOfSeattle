d268c84fc4e4b95bc6b8ad6a0504ab0716826a27
tcandens <joey.thies@gmail.com>
2016-01-28 12:59:27 -0800

    Switch to .jsx extensions.
    Should encourage being explicit and purposeful about
    component creation and separation.

--- web/src/app.js
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-
-import Hello from './tester'
-
-ReactDOM.render(<Hello />, document.getElementById('root'));
---
+++ web/src/app.jsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import Hello from './tester.jsx'
+
+ReactDOM.render(<Hello word='Albert'/>, document.getElementById('root'));
