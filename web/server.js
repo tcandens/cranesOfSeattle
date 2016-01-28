3cc28dd1d8aa9acb0c260787afd478137b9ff609
tcandens <joey.thies@gmail.com>
2016-01-28 10:23:04 -0800

    Tweak to webpack-dev-server.
    Using specific hosts and ports in webpack config allows using
    dev-server behind a Docker container. Also note that
    React-Hot-Loader only works when components adhere to certain
    signatures.

--- web/server.js
+++ web/server.js
@@ -2,13 +2,15 @@ const webpack = require('webpack');
 const WebpackDevServer = require('webpack-dev-server');
 const config = require('./webpack.development.js');
 
+const PORT = 9000;
+
 new WebpackDevServer(webpack(config), {
   publicPath: config.output.publicPath,
   hot: true,
-  historyApiFallback: true,
-}).listen(9000, '0.0.0.0', (error, result) => {
+  historyApiFallback: true
+}).listen(PORT, '0.0.0.0', (error, result) => {
     if (error) {
       console.log(error);
     }
-    console.log('Listening to Webpack-Dev-Server at localhost:9000');
+    console.log('Listening to Webpack-Dev-Server at localhost:' + PORT);
   });
--- web/src/tester.js
+++ web/src/tester.js
@@ -1,34 +1,11 @@
-const helloFactory = function ({ React }) {
-  return function Hello(props) {
-    Hello.propTypes = {
-      word: React.PropTypes.string
-    };
+import React, { Component } from 'react';
 
-    return {
-      props,
-      componentDidUpdate() {
-        this.refs.wordInput.getDOMNode().focus();
-      },
-      render() {
+class helloComponent extends Component {
+  render() {
+    return (
+      <h1>Fuck off, world</h1>
+    )
+  }
+}
 
-        const onKeyUp = e => {
-          if (e.key !== 'Enter') return;
-          this.props.word = e.target.value;
-          this.render();
-        };
-
-        return (
-          <p>Fuck you,&nbsp;
-            <span>Fucker</span>
-            <input
-              ref='wordInput'
-              placeholder={this.props.word}
-              onKeyUp={onKeyUp} />
-          </p>
-        );
-      }
-    };
-  };
-};
-
-export default helloFactory;
+export default helloComponent
