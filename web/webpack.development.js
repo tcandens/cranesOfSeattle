e928d972abd3fbba362e7a8efb0296be20f32fb6
tcandens <joey.thies@gmail.com>
2016-01-30 12:52:50 -0800

    Replace app entrypoint and add css-loaders.

--- web/webpack.development.js
+++ web/webpack.development.js
@@ -5,7 +5,7 @@ module.exports = {
   devtool: 'cheap-module-eval-source-map',
   entry: [
     'webpack-hot-middleware/client?reload=true',
-    './src/app.jsx'
+    './src/index.jsx'
   ],
   output: {
     path: path.join(__dirname, 'dist/js'),
@@ -17,10 +17,16 @@ module.exports = {
     new webpack.NoErrorsPlugin()
   ],
   module: {
-    loaders: [{
-      test: /\.jsx$/,
-      loaders: ['babel'],
-      include: path.join(__dirname, 'src')
-    }]
+    loaders: [
+      {
+        test: /\.jsx$/,
+        loaders: ['babel'],
+        include: path.join(__dirname, 'src')
+      },
+      {
+        test: /\.css$/,
+        loaders: ['style', 'css']
+      }
+    ]
   }
 };
