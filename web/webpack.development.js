7fb76aa0604dc8b77a8c70bfc0b6c1ab84882923
tcandens <joey.thies@gmail.com>
2016-01-30 21:23:13 -0800

    Use protected imports for mapbox access key

--- web/webpack.development.js
+++ web/webpack.development.js
@@ -19,7 +19,8 @@ module.exports = {
   resolve: {
     alias: {
       'mapbox-gl/css': path.resolve('./node_modules/mapbox-gl/dist/mapbox-gl.css'),
-      'mapbox-gl': path.resolve('./node_modules/mapbox-gl/dist/mapbox-gl.js')
+      'mapbox-gl': path.resolve('./node_modules/mapbox-gl/dist/mapbox-gl.js'),
+      'protected': path.resolve(__dirname, 'protected.js')
     }
   },
   module: {
