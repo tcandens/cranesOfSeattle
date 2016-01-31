8233430f984cb4d035d8585e9bcd9a2005d72a3c
tcandens <joey.thies@gmail.com>
2016-01-30 19:58:39 -0800

    Add dependancies

--- web/webpack.development.js
+++ web/webpack.development.js
@@ -16,16 +16,30 @@ module.exports = {
     new webpack.HotModuleReplacementPlugin(),
     new webpack.NoErrorsPlugin()
   ],
+  resolve: {
+    alias: {
+      'mapbox-gl/css': path.resolve('./node_modules/mapbox-gl/dist/mapbox-gl.css'),
+      'mapbox-gl': path.resolve('./node_modules/mapbox-gl/dist/mapbox-gl.js')
+    }
+  },
   module: {
     loaders: [
       {
-        test: /\.jsx$/,
+        test: /\.js/,
         loaders: ['babel'],
         include: path.join(__dirname, 'src')
       },
       {
         test: /\.css$/,
         loaders: ['style', 'css']
+      },
+      {
+        test: /\.styl$/,
+        loaders: ['style', 'css', 'stylus']
+      },
+      {
+        test: /\.json$/,
+        loaders: ['json']
       }
     ]
   }
