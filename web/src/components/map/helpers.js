8c1e265fe867d6cb71da354352af77b010040391
tcandens <joey.thies@gmail.com>
2016-02-12 11:57:36 -0800

    Tweak map createLayer helper.
    Creates a more simplistic layer view.

--- web/src/components/map/helpers.js
+++ web/src/components/map/helpers.js
@@ -4,12 +4,9 @@ export function createLayer(sourceName) {
     'type': 'symbol',
     'source': sourceName,
     'layout': {
-      'icon-image': 'default_marker',
+      'icon-image': 'crane',
       'icon-allow-overlap': 'true',
-      'icon-ignore-placement': 'true',
-      'text-field': 'Report',
-      'text-anchor': 'top',
-      'text-font': ['Open Sans Semibold', 'Arial Unicode MS Bold']
+      'icon-ignore-placement': 'true'
     }
   };
 }
