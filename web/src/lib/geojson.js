ce3cd386679c3e05f0f400e0feb4aa818874cfb2
tcandens <joey.thies@gmail.com>
2016-03-13 15:06:15 -0700

    Restructure lib/geojson

--- web/src/lib/geojson.js
+++ web/src/lib/geojson.js
@@ -1,18 +1,18 @@
-const geojson = {};
-
-geojson.pointFromLngLat = (lnglat) => {
-  const longitude = lnglat.longitude || lnglat.lng;
-  const latitude = lnglat.latitude || lnglat.lat;
-  return {
-    'type': 'Feature',
-    'geometry': {
-      'type': 'Point',
-      'coordinates': [longitude, latitude]
-    },
-    'properties': {
-      'user_id': 11
-    }
-  };
+const geojson = {
+  pointFromLngLat: (lnglat) => {
+    const longitude = lnglat.longitude || lnglat.lng;
+    const latitude = lnglat.latitude || lnglat.lat;
+    return {
+      'type': 'Feature',
+      'geometry': {
+        'type': 'Point',
+        'coordinates': [longitude, latitude]
+      },
+      'properties': {
+        'user_id': 11
+      }
+    };
+  }
 };
 
 export default geojson;
