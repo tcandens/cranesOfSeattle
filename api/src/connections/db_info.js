fb81fa9859fdb8170bd867753593f9b23ac036b9
tcandens <joey.thies@gmail.com>
2016-03-30 18:28:01 -0700

    Use linked name instead of environmental variables
    Since docker-compose does not populate env_vars since 1.6.x upgrade,
    use preferred method of services as named tcp addresses.

--- api/src/connections/db_info.js
+++ api/src/connections/db_info.js
@@ -1,5 +1,5 @@
 export default {
-  host: process.env.DB_PORT_5432_TCP_ADDR || 'localhost',
+  host: process.env.DB_PORT_5432_TCP_ADDR || 'db',
   port: process.env.DB_PORT_5432_TCP_PORT || 5432,
   database: process.env.PGDATABASE || 'postgres',
   user: process.env.PGUSER || 'postgres',
