98010ee6b71166c2cfa59a6e4d97d83941c810dc
tcandens <joey.thies@gmail.com>
2016-04-07 23:26:13 -0700

    Webpack codesplitting and React Router dynamic routing are working
    Boilerplate for require.ensure cannot be abstracted out of routes, due to Webpack statically analyzing these as split points for chunks. Not a problem since only the map and perhaps another module should require lazy loading.

--- web/src/routes/index.js
+++ web/src/routes/index.js
@@ -3,19 +3,16 @@ import {Route, IndexRoute} from 'react-router';
 
 import MainLayout from 'layouts/main';
 import Entry from 'components/entry';
-import MapContainer from 'containers/map';
-
-const requireComponent = (path) => (location, callback) => {
-  require.ensure([], require => {
-    callback(null, require(path));
-  });
-};
 
 export default (store) => {
   return (
     <Route path='/' component={MainLayout}>
       <IndexRoute component={Entry} />
-      <Route path='map' getComponent={requireComponent('containers/map')} />
+      <Route path='map' getComponent={(location, callback) => {
+        require.ensure([], require => {
+          callback(null, require('containers/map').default);
+        });
+      }} />
     </Route>
   );
 };
