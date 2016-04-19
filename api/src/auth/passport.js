c7ec21b6ebee2bb27bd5bc323c5b65f70c4089ab
tcandens <joey.thies@gmail.com>
2016-04-19 15:48:37 -0700

    Use environment variables for HOST when build OAuth callback endpoints

--- api/src/auth/passport.js
+++ api/src/auth/passport.js
@@ -1,10 +1,11 @@
 import passport from 'koa-passport';
 import GooglePassport from 'passport-google-oauth20';
 import dotenv from 'dotenv';
+import url from 'url';
 dotenv.config();
 
 const Strategy = GooglePassport.Strategy;
-const {GOOGLE_OAUTH_ID, GOOGLE_OAUTH_SECRET} = process.env;
+const {HOST, GOOGLE_OAUTH_ID, GOOGLE_OAUTH_SECRET} = process.env;
 
 passport.serializeUser((user, done) => {
   done(null, user);
@@ -17,7 +18,7 @@ passport.deserializeUser((id, done) => {
 passport.use(new Strategy({
     clientID: GOOGLE_OAUTH_ID,
     clientSecret: GOOGLE_OAUTH_SECRET,
-    callbackURL: 'http://localhost:8080/api/auth/google/callback'
+    callbackURL: url.resolve(HOST, '/api/auth/google/callback')
   },
   (token, tokenSecret, profile, done) => {
     return done(null, profile._json);
