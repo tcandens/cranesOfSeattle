99fa8cded37446669b9a878fa60d8edd4dd37a22
tcandens <joey.thies@gmail.com>
2016-04-26 16:46:05 -0700

    Separate modules for OAuth popup lib

--- web/src/lib/oauth/index.js
+++ web/src/lib/oauth/index.js
@@ -1,21 +1,20 @@
 import Promise from 'bluebird';
-import queryString from 'query-string';
-import axios from 'axios';
+import {
+  listenForToken,
+  fetchGoogleProfile,
+  createGoogleTokenUrl
+} from './utils';
+
+const POPUP_NAME = 'Sign In Popup';
+const GOOGLE_OAUTH_WINDOW_FEATURES = `
+  width=320,
+  height=570,
+  scrollbars=no,
+  locationbar=no
+`;
 
 export function loginPopup() {
-  const POPUP_NAME = 'Sign In Popup';
-  const GOOGLE_OAUTH_WINDOW_FEATURES = `
-    width=320,
-    height=570,
-    scrollbars=no,
-    locationbar=no
-  `;
-  const authUrl = createGoogleTokenUrl({
-    redirect_uri: `${window.location.host}/api/auth/google/callback`,
-    response_type: 'token',
-    state: 'profile',
-    scope: 'profile'
-  });
+  const authUrl = createGoogleTokenUrl();
   const popup = window.open(authUrl, POPUP_NAME, GOOGLE_OAUTH_WINDOW_FEATURES);
   return listenForToken(popup).then(token => {
     return fetchGoogleProfile(token);
@@ -24,51 +23,4 @@ export function loginPopup() {
   }).catch(error => {
     return Promise.reject(error);
   });
-}
-
-function fetchGoogleProfile(token) {
-  const googleApiUrl = 'https://www.googleapis.com/userinfo/v2/me';
-  return axios.get(googleApiUrl, {
-    headers: {
-      'Authorization': `Bearer ${token}`
-    }
-  }).then(response => {
-    return Promise.resolve(response.data);
-  });
-}
-
-function createGoogleTokenUrl(queries) {
-  queries = queries || {};
-  queries['client_id'] = GOOGLE_CLIENT_ID;
-  const protocol = window.location.protocol;
-  if (queries.redirect_uri) {
-    queries.redirect_uri = `${protocol}//${queries.redirect_uri}`;
-  }
-  const qstring = queryString.stringify(queries);
-  return `https://accounts.google.com/o/oauth2/v2/auth?${qstring}`;
-}
-
-function listenForToken(popup) {
-  return new Promise(resolve => {
-    let parsed;
-    function tryParsing(popup) {
-      try {
-        parsed = parsePopupLocation(popup);
-      } catch(e) {}
-      if (parsed && parsed['access_token']) {
-        resolve(parsed.access_token);
-        window.clearTimeout(window.CRANES_TIMER);
-        popup.close();
-      } else {
-        window.CRANES_TIMER = setTimeout(() => {
-          tryParsing(popup);
-        }, 500);
-      }
-    }
-    tryParsing(popup);
-  });
-}
-
-function parsePopupLocation(popup) {
-  return queryString.parse(popup.location.hash);
 }
