ca60c02d1c09857a2853ac01551fbd26a656be5a
tcandens <joey.thies@gmail.com>
2016-04-26 16:55:52 -0700

    Tear out server side auth
    Removed passport and passport routes decorator.

--- api/src/routers/auth.js
+++ api/src/routers/auth.js
@@ -1,21 +1,9 @@
 import Router from 'koa-router';
 
-export default (passport) => {
-  return Router()
-    .get('/auth/google', passport.authenticate(
-      'google',
-      {
-        scope: ['profile']
-      }
-    ))
-    .get('/auth/google/callback',
-      passport.authenticate('google', {failureRedirect: '/login'}),
-      async (ctx) => {
-        await ctx.redirect('/api/auth/received?id=' + ctx.req.user.id);
-      }
-    )
-    .get('/auth/received', async (ctx) => {
-      ctx.type = 'html';
-      ctx.body = '<h1>Redirecting.</h1><p>This popup should close soon.</p>';
-    });
-}
+export default Router()
+  .get('/auth/google/callback', (ctx) => {
+    ctx.body = `
+      <h1>Authorized.</h1>
+      <h2>Redirecting...</h2>
+    `;
+  })
