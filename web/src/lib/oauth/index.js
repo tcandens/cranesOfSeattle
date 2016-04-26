b16ae2a235296d0d128834a4c76ab5c294201f81
tcandens <joey.thies@gmail.com>
2016-04-26 16:20:05 -0700

    Fetch full Google OAuth2 User Profile

--- web/src/lib/oauth/index.js
+++ web/src/lib/oauth/index.js
@@ -1,26 +1,15 @@
 import Promise from 'bluebird';
 import queryString from 'query-string';
-
-const POPUP_NAME = 'Sign In Popup';
-const GOOGLE_OAUTH_WINDOW_FEATURES = `
-  width=320,
-  height=570,
-  scrollbars=no,
-  locationbar=no
-`;
-
-export function createGoogleTokenUrl(queries) {
-  queries = queries || {};
-  queries['client_id'] = GOOGLE_CLIENT_ID;
-  const protocol = window.location.protocol;
-  if (queries.redirect_uri) {
-    queries.redirect_uri = `${protocol}//${queries.redirect_uri}`;
-  }
-  const qstring = queryString.stringify(queries);
-  return `https://accounts.google.com/o/oauth2/v2/auth?${qstring}`;
-}
+import axios from 'axios';
 
 export function loginPopup() {
+  const POPUP_NAME = 'Sign In Popup';
+  const GOOGLE_OAUTH_WINDOW_FEATURES = `
+    width=320,
+    height=570,
+    scrollbars=no,
+    locationbar=no
+  `;
   const authUrl = createGoogleTokenUrl({
     redirect_uri: `${window.location.host}/api/auth/google/callback`,
     response_type: 'token',
@@ -29,8 +18,32 @@ export function loginPopup() {
   });
   const popup = window.open(authUrl, POPUP_NAME, GOOGLE_OAUTH_WINDOW_FEATURES);
   listenForToken(popup).then(token => {
-    console.log(token);
+    return fetchGoogleProfile(token)
+  }).then(profile => {
+    console.log(profile);
   });
+}
+
+function fetchGoogleProfile(token) {
+  const googleApiUrl = 'https://www.googleapis.com/userinfo/v2/me';
+  return axios.get(googleApiUrl, {
+    headers: {
+      'Authorization': `Bearer ${token}`
+    }
+  }).then(response => {
+    return Promise.resolve(response.data);
+  });
+}
+
+function createGoogleTokenUrl(queries) {
+  queries = queries || {};
+  queries['client_id'] = GOOGLE_CLIENT_ID;
+  const protocol = window.location.protocol;
+  if (queries.redirect_uri) {
+    queries.redirect_uri = `${protocol}//${queries.redirect_uri}`;
+  }
+  const qstring = queryString.stringify(queries);
+  return `https://accounts.google.com/o/oauth2/v2/auth?${qstring}`;
 }
 
 function listenForToken(popup) {
