efcf70d433951500860d93b456c46acabf0c8bb0
tcandens <joey.thies@gmail.com>
2016-05-21 00:24:50 -0700

    Add private scripts to .gitignore, Add svg loader to webpack

--- web/webpack/loaders.js
+++ web/webpack/loaders.js
@@ -4,17 +4,17 @@ const ExtractTextPlugin = require('extract-text-webpack-plugin');
 const createLoaders = (ROOT, isDeveloping) => {
   return [
     {
-      test: /\.js/,
+      test: /\.jsx?$/,
       loaders: ['babel'],
       include: path.resolve(ROOT, 'src'),
-      exclude: /(node_modules)/
+      exclude: /(node_modules)/,
     },
     {
       test: /\.css$/,
       loader: (isDeveloping ?
         'style!css' :
         ExtractTextPlugin.extract(['style', 'css'])
-      )
+      ),
     },
     {
       test: /\.styl$/,
@@ -22,22 +22,27 @@ const createLoaders = (ROOT, isDeveloping) => {
         'style!css!stylus' :
         ExtractTextPlugin.extract(['css', 'stylus'])
       ),
-      include: path.join(ROOT, 'src')
+      include: path.join(ROOT, 'src'),
     },
     {
       test: /\.json$/,
-      loaders: ['json']
+      loaders: ['json'],
     },
     {
       test: /\.(jpe?g)$/,
       loader: 'url?.[ext]&mimetype=image/jpeg',
-      include: path.join(ROOT, 'assets')
+      include: path.join(ROOT, 'assets'),
     },
     {
       test: /\.jade$/,
       loaders: ['jade'],
-      include: path.join(ROOT, 'templates')
-    }
+      include: path.join(ROOT, 'templates'),
+    },
+    {
+      test: /\.svg$/,
+      loaders: ['url?.[ext]&mimetype=image/svg'],
+      include: path.join(ROOT, 'assets'),
+    },
   ];
 };
 
