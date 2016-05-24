8fbf220d1012faebc0a0e65c62dc816918ac8135
tcandens <joey.thies@gmail.com>
2016-05-24 13:56:45 -0700

    Try to get CI working, travis tests still erroring out

--- api/entry.sh
+++ api/entry.sh
@@ -6,16 +6,16 @@ export DATABASE_URL=db://${PGUSER}:${PGPASSWORD}@db/${PGDATABASE}
 
 if [ "$ENV" = 'DEV' ]; then
   echo $'Running Development Server\n--------------------------'
-  ./node_modules/.bin/pg-migrate up;
+  npm run migrate:up;
   npm start;
 elif [ "$ENV" = 'TEST' ]; then
   export PORT=3333
   echo $'Running Tests\n-------------';
-  ./node_modules/.bin/pg-migrate up;
+  npm run migrate:up;
   npm run tests;
 else
   echo "Running Production Server"
   echo "-------------------------"
   # Change this to PM2 or Forever task
-  npm run production
+  npm run production;
 fi
