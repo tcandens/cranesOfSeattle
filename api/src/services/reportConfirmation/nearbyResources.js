33e5eda2fb8927e53c0c9de627b16acda7fdda8e
tcandens <joey.thies@gmail.com>
2016-06-30 00:04:17 -0700

    Clean up nearbyResources
    Remove deps and disambiguate constants for exporting to tests.

--- api/src/services/reportConfirmation/nearbyResources.js
+++ api/src/services/reportConfirmation/nearbyResources.js
@@ -1,12 +1,12 @@
-import Promise from 'bluebird';
-import defaults from 'lodash/defaults';
 import craneModel from '../../resources/cranes/model';
 import reportModel from '../../resources/reports/model';
 import permitModel from '../../resources/permits/model';
 import geolib from 'geolib';
 
+export const RADIUS = 60;
+
 const defaultOptions = {
-  radius: 60
+  radius: RADIUS
 }
 
 /**
