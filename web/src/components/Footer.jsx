490d134254e380648be6a60b9831a696daf108f7
tcandens <joey.thies@gmail.com>
2016-06-29 17:10:12 -0700

    Add intro info and footer

---
+++ web/src/components/Footer.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default function Footer() {
+  return (
+    <footer className="c-footer">
+      <a href="https://www.github.com/tcandens/cranesOfSeattle">
+        View on Github
+      </a>
+    </footer>
+  );
+}
