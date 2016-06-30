76f9414af11169cd8c3c89da264c323c9c51b7de
tcandens <joey.thies@gmail.com>
2016-06-29 18:54:03 -0700

    Add basic tooltip walkthrough to exploring container

---
+++ web/src/components/Tooltips.jsx
@@ -0,0 +1,50 @@
+import React, {Component} from 'react';
+import Button from 'components/Button';
+
+export default class Tooltips extends Component {
+  state = {
+    reticle: true,
+  }
+  handleWalkthrough = (tool) => {
+    const {closeAction} = this.props;
+    const toolOrder = [
+      'reticle',
+      'markers',
+    ];
+    const nextTool = toolOrder[toolOrder.indexOf(tool) + 1];
+    this.setState({
+      [tool]: false,
+      [nextTool]: true,
+    });
+    if (!nextTool) closeAction();
+  }
+  render = () => {
+    return (
+      <section className="c-tooltips">
+        {this.state.reticle &&
+          <div className="c-tooltips--reticle">
+            <h2>You are here.</h2>
+            <Button
+              className="button--dark"
+              onClick={() => this.handleWalkthrough('reticle')}
+            >
+              got it
+            </Button>
+          </div>
+        }
+        {this.state.markers &&
+          <div className="c-tooltips--markers">
+            <h2>The color of a report shows its confidence</h2>
+            <div className="c-tooltips--confidence-scale"></div>
+            <Button
+              className="button--dark"
+              onClick={() => this.handleWalkthrough('markers')}
+            >
+              got it
+            </Button>
+          </div>
+        }
+      </section>
+    );
+  }
+}
