94e17eef7a438d18ff33e48030853ddfdc2a4074
tcandens <joey.thies@gmail.com>
2016-08-05 17:31:02 -0700

    Add router transitions with transitionGroup

--- web/src/layouts/Main.jsx
+++ web/src/layouts/Main.jsx
@@ -1,10 +1,21 @@
 import React from 'react';
 import Navigation from 'components/Navigation';
+import ReactCSSTransitionGroup from 'react-addons-css-transition-group';
 
-const MainLayout = (props) => {
+const MainLayout = ({children, location}) => {
   return (
     <main className="l-main">
-      {props.children}
+      <ReactCSSTransitionGroup
+        component="div"
+        className="transition-container"
+        transitionName="layout"
+        transitionEnterTimeout={400}
+        transitionLeaveTimeout={400}
+      >
+        {React.cloneElement(children, {
+          key: location.pathname,
+        })}
+      </ReactCSSTransitionGroup>
       <Navigation />
     </main>
   );
